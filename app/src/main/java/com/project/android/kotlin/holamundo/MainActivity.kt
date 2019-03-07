8c431971dcbab2066c18de26ee29e76d5012684a
jorgevc87 <lacunacoil18@hotmail.com>
2019-03-07 14:45:07 -0500

    primer commit

---
+++ app/src/main/java/com/project/android/kotlin/holamundo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.project.android.kotlin.holamundo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
